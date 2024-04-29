fb20b4f5028d12e4171377647be701b90f30b267
netlemon <netlemon2021@gmail.com>
2024-04-29 13:23:54 +0300

    feat: added mw context resolver

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,10 @@ use crate::model::ModelController;
 
 // Re-export
 pub use self::error::{Error, Result};
+mod ctx;
 mod error;
-
-mod web;
 mod model;
+mod web;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -26,13 +26,17 @@ async fn main() -> Result<()> {
 
     let routes_api = web::routes_tickets::routes(mc.clone())
         .route_layer(middleware::from_fn(web::mw_auth::mw_require_auth));
-        
+
     let routes_all = Router::new()
         .merge(routes_hello())
         .merge(web::routes_login::routes())
         .nest("/api", routes_api)
         // Layers are executed from bottom to top
         .layer(middleware::map_response(main_response_mapper))
+        .layer(middleware::from_fn_with_state(
+            mc.clone(),
+            web::mw_auth::mw_ctx_resolver,
+        ))
         .layer(CookieManagerLayer::new())
         .fallback_service(routes_static());
 
--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,22 +1,63 @@
+use axum::extract::FromRequestParts;
+use axum::http::request::Parts;
+use axum::{async_trait, RequestPartsExt};
 use axum::{body::Body, http::Request, middleware::Next, response::Response};
 use lazy_regex::regex_captures;
-use tower_cookies::Cookies;
+use tower_cookies::{Cookie, Cookies};
 
+use crate::ctx::Ctx;
 use crate::web::AUTH_TOKEN;
 use crate::{Error, Result};
 
-pub async fn mw_require_auth(cookies: Cookies, req: Request<Body>, next: Next) -> Result<Response> {
+pub async fn mw_require_auth(ctx: Result<Ctx>, req: Request<Body>, next: Next) -> Result<Response> {
     println!("->> {:<12} - mw_require_auth", "MIDDLEWARE");
-    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
 
-    // TODO: real auth-token parsing & validation
-    let (user_id, exp, sign) = auth_token
-        .ok_or(Error::AuthFailNoAuthTokenCookie)
-        .and_then(parse_token)?;
-
-    // TODO: token component validation
+    ctx?;
 
     Ok(next.run(req).await)
+}
+
+pub async fn mw_ctx_resolver(
+    cookies: Cookies,
+    mut req: Request<Body>,
+    next: Next,
+) -> Result<Response> {
+    println!("->> {:<12} - mw_ctx_resolver", "MIDDLEWARE");
+
+    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+
+    // Compute Result<Ctx>
+    let result_ctx = match auth_token
+        .ok_or(Error::AuthFailNoAuthTokenCookie)
+        .and_then(parse_token)
+    {
+        Ok((user_id, _exp, _sign)) => Ok(Ctx::new(user_id)),
+        Err(e) => Err(e),
+    };
+
+    // Remove the cookie if something is wrong other than NoAuthTokenCookie
+    if result_ctx.is_err() && !matches!(result_ctx, Err(Error::AuthFailNoAuthTokenCookie)) {
+        cookies.remove(Cookie::from(AUTH_TOKEN))
+    }
+
+    req.extensions_mut().insert(result_ctx);
+
+    Ok(next.run(req).await)
+}
+
+#[async_trait]
+impl<S: Send + Sync> FromRequestParts<S> for Ctx {
+    type Rejection = Error;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
+        println!("->> {:<12} - CTX", "EXTRACTOR");
+
+        parts
+            .extensions
+            .get::<Result<Ctx>>()
+            .ok_or(Error::AuthFailCtxNotInRequestExt)?
+            .clone()
+    }
 }
 
 /// Parse a token of format `user-[user-id].[expiration].[signature]`
